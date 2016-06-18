24ba466399f4225fb79c1f73c56e810e765a16e3
WEIBIN SONG <sone@silmoon.com>
2016-06-18 17:54:51 +0000

    git-svn-id: https://svn.silmoon.com:8443/svn/SilmoonClassLib@11 1573403e-aac7-6b4a-8685-2b6d39f8d130

--- SilmoonClassLib/Net/Sockets/MacAddress.cs
+++ SilmoonClassLib/Net/Sockets/MacAddress.cs
@@ -30,5 +30,43 @@ namespace Silmoon.Net.Sockets
         {
             return a0.ToString("X") + a1.ToString("X") + a2.ToString("X") + a3.ToString("X") + a4.ToString("X") + a5.ToString("X");
         }
+
+        public static bool operator ==(MacAddress c1, MacAddress c2)
+        {
+            if (c1.a0 == c2.a0 &&
+               c1.a1 == c2.a1 &&
+               c1.a2 == c2.a2 &&
+               c1.a3 == c2.a3 &&
+               c1.a4 == c2.a4 &&
+               c1.a5 == c2.a5)
+                return true;
+            else return false;
+        }
+        public static bool operator !=(MacAddress c1, MacAddress c2)
+        {
+            if (c1.a0 == c2.a0 &&
+               c1.a1 == c2.a1 &&
+               c1.a2 == c2.a2 &&
+               c1.a3 == c2.a3 &&
+               c1.a4 == c2.a4 &&
+               c1.a5 == c2.a5)
+                return false;
+            else return true;
+        }
+
+        public static MacAddress BroadcastAddress
+        {
+            get
+            {
+                MacAddress mac;
+                mac.a0 = 0xff;
+                mac.a1 = 0xff;
+                mac.a2 = 0xff;
+                mac.a3 = 0xff;
+                mac.a4 = 0xff;
+                mac.a5 = 0xff;
+                return mac;
+            }
+        }
     }
 }
