2369d726688d446d6ba0797e1c145a836da8fd26
WEIBIN SONG <sone@silmoon.com>
2019-06-05 20:39:44 +0000

    git-svn-id: https://svn.silmoon.com:8443/svn/SilmoonClassLib@45 1573403e-aac7-6b4a-8685-2b6d39f8d130

---
+++ SilmoonClassLib/Copy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Silmoon
+{
+    public class Copy
+    {
+        public static object MemberCopy(object from, object to)
+        {
+            Type tto = to.GetType();
+
+            var p = from.GetType().GetProperties();
+            foreach (var item in p)
+            {
+                tto.GetProperty(item.Name).SetValue(to, item.GetValue(from, null), null);
+            }
+            return to;
+        }
+        public static T New<T>(object obj) where T : new()
+        {
+            T t = new T();
+            MemberCopy(obj, t);
+
+            return t;
+        }
+
+    }
+}
