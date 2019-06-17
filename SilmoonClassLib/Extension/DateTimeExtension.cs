5a10b8e4f3c0d7c3c0267e23ec3ae66cc1445eb4
WEIBIN SONG <sone@silmoon.com>
2019-06-17 10:04:08 +0000

    git-svn-id: https://svn.silmoon.com:8443/svn/SilmoonClassLib@48 1573403e-aac7-6b4a-8685-2b6d39f8d130

---
+++ SilmoonClassLib/Extension/DateTimeExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Silmoon.Extension
+{
+    public static class DateTimeExtension
+    {
+        public static string GetUnixStyleTimeStamp(this DateTime datetime)
+        {
+            return SpecialConverter.GET_UNIX_TIMESTAMP(datetime).ToString();
+        }
+    }
+}
---
+++ SilmoonClassLib/Extension/XmlHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace Silmoon.Extension
+{
+    public static class XmlHelper
+    {
+        public static string ToXmlString(this XmlDocument xml)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                xml.Save(ms);
+                ms.Position = 0;
+                using (StreamReader sr = new StreamReader(ms))
+                {
+                    string resp = sr.ReadToEnd();
+                    return resp;
+                }
+            }
+        }
+    }
+}
