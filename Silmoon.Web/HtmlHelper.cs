99a3df180bcbb5d0ce6fbae61aff12911c5d7023
WEIBIN SONG <sone@silmoon.com>
2020-03-22 07:17:17 +0000

    git-svn-id: https://svn.silmoon.com:8443/svn/SilmoonClassLib@77 1573403e-aac7-6b4a-8685-2b6d39f8d130

--- Silmoon.Web/HtmlHelper.cs
+++ Silmoon.Web/HtmlHelper.cs
@@ -4,6 +4,7 @@ using System.Collections.Specialized;
 using System.Dynamic;
 using System.Text;
 using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
 
 namespace Silmoon.Web
 {
@@ -153,24 +154,6 @@ namespace Silmoon.Web
             if (submit) result += "<script>document.forms['" + name + "'].submit();</script>";
             return result;
         }
-        public static List<SelectListItem> MakeMvcSelectListItems(Type type, object enumObj)
-        {
-            if (type.IsEnum)
-            {
-                var names = type.GetEnumNames();
-                var values = type.GetEnumValues();
-                var result = new List<SelectListItem>();
-                for (int i = 0; i < names.Length; i++)
-                {
-                    result.Add(new SelectListItem() { Text = names[i], Value = values.GetValue(i).ToString() });
-                }
-                return result;
-            }
-            else
-            {
-                return null;
-            }
-        }
         public static string MakeNewQueryString(NameValueCollection collection, string additionQueryString = "")
         {
             collection = new NameValueCollection(collection);
@@ -200,6 +183,39 @@ namespace Silmoon.Web
             //    s = "?" + s;
 
             return s;
+        }
+
+
+        public static List<SelectListItem> MakeMvcSelectListItems(Type type, object enumObj)
+        {
+            if (type.IsEnum)
+            {
+                var names = type.GetEnumNames();
+                var values = type.GetEnumValues();
+                var result = new List<SelectListItem>();
+                for (int i = 0; i < names.Length; i++)
+                {
+                    result.Add(new SelectListItem() { Text = names[i], Value = values.GetValue(i).ToString() });
+                }
+                return result;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public static string GetAjaxOptionJavascriptFunctions()
+        {
+            return js.ajaxOptionFunctions;
+        }
+        public static AjaxOptions GetAjaxOptions(string senderId, string onSuccess = "null", string onFailed = "null", string onError = "null")
+        {
+            var result = new AjaxOptions()
+            {
+                OnBegin = "(function(sender){ _ajax_on_begin(sender); })('" + senderId + "')",
+                OnComplete = "(function(senderId, e, onSuccess, onFailed, onError){ _ajax_on_complete(senderId, e, onSuccess, onFailed, onError); })('" + senderId + "', arguments[0], " + onSuccess + ", " + onFailed + ", " + onError + ")"
+            };
+            return result;
         }
     }
 }
---
+++ Silmoon.Web/Resources/ajaxOptionFunctions.js
@@ -0,0 +1,21 @@
+﻿
+function _ajax_on_complete(senderId, e, onSuccess, onFailed, onError) {
+    if (e.readyState == 4) {
+        var obj = JSON.parse(e.responseText);
+        if (obj.Success) {
+            $("#" + senderId).html('成功');
+            if (onSuccess != null) onSuccess(senderId, e);
+        } else {
+            $("#" + senderId).html('失败')
+            if (onFailed != null) onFailed(senderId, e);
+        }
+    }
+    else {
+        $("#" + senderId).html('错误')
+        if (onError != null) onError(senderId, e);
+    }
+}
+
+function _ajax_on_begin(senderId) {
+    $("#" + senderId).html("请求中...");
+}
