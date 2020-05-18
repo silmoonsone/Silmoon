e28d76928e983ceb85004684c46822d29a957810
WEIBIN SONG <sone@silmoon.com>
2020-05-18 17:44:26 +0000

    git-svn-id: https://svn.silmoon.com:8443/svn/SilmoonClassLib@100 1573403e-aac7-6b4a-8685-2b6d39f8d130

---
+++ Silmoon.Extension/StringExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Silmoon.Extension
+{
+    public static class StringExtension
+    {
+        public static bool IsNullOrEmpty(this string s)
+        {
+            return string.IsNullOrEmpty(s);
+        }
+    }
+}
