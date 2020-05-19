bab00e768da5763f4a3a2747005fa948b5927faa
WEIBIN SONG <sone@silmoon.com>
2020-05-19 18:02:54 +0000

    git-svn-id: https://svn.silmoon.com:8443/svn/SilmoonClassLib@103 1573403e-aac7-6b4a-8685-2b6d39f8d130

--- Silmoon.Extension/Json/JsonExtension.cs
+++ Silmoon.Extension/Json/JsonExtension.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Silmoon.Extension.Json
@@ -10,6 +11,25 @@ namespace Silmoon.Extension.Json
         public static string ToJsonString(this object obj)
         {
             return JsonConvert.SerializeObject(obj);
+        }
+        public static string ToFormattedJsonString(this object obj)
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            if (obj != null)
+            {
+                using (StringWriter textWriter = new StringWriter())
+                {
+                    using (JsonTextWriter jsonWriter = new JsonTextWriter(textWriter) { Formatting = Formatting.Indented, Indentation = 4, IndentChar = ' ' })
+                    {
+                        serializer.Serialize(jsonWriter, obj);
+                        return textWriter.ToString();
+                    }
+                }
+            }
+            else
+            {
+                return "null";
+            }
         }
     }
 }
