90e81b9e98d9fb7d813ebb0cb76c830253204cc5
WEIBIN SONG <sone@silmoon.com>
2020-05-29 16:48:44 +0000

    git-svn-id: https://svn.silmoon.com:8443/svn/SilmoonClassLib@112 1573403e-aac7-6b4a-8685-2b6d39f8d130

--- Silmoon.Data/QueryModel/Sort.cs
+++ Silmoon.Data/QueryModel/Sort.cs
@@ -8,6 +8,15 @@ namespace Silmoon.Data.QueryModel
 {
     public class Sort
     {
+        public Sort()
+        {
+
+        }
+        public Sort(string name, SortMethod method)
+        {
+            Name = name;
+            Method = method;
+        }
         public string Name { get; set; }
         public SortMethod Method { get; set; }
 
