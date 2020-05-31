1ae88c41c77f9034a6eac4e957096282c8446b2f
WEIBIN SONG <sone@silmoon.com>
2020-05-31 08:07:43 +0000

    git-svn-id: https://svn.silmoon.com:8443/svn/SilmoonClassLib@120 1573403e-aac7-6b4a-8685-2b6d39f8d130

--- Silmoon.Web/Properties/AssemblyInfo.cs
+++ Silmoon.Web/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("Silmoon.Web")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Build 191121")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Silmoon.Web")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("威海银之月信息技术有限公司")]
+[assembly: AssemblyProduct("银月(SILMOON)通用类型库")]
+[assembly: AssemblyCopyright("版权所有 (C) SILMOON 2019")]
+[assembly: AssemblyTrademark("SILMOON INC.")]
 [assembly: AssemblyCulture("")]
 
 // 将 ComVisible 设置为 false 会使此程序集中的类型
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.26")]
+[assembly: AssemblyFileVersion("0.0.4.5")]
