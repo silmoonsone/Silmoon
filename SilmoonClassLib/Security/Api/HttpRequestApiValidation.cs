595d1c0aadecee8060fc159ce20aca486afac898
WEIBIN SONG <sone@silmoon.com>
2020-07-25 13:19:53 +0000

    git-svn-id: https://svn.silmoon.com:8443/svn/SilmoonClassLib@136 1573403e-aac7-6b4a-8685-2b6d39f8d130

---
+++ SilmoonClassLib/Security/Api/HttpRequestApiValidation.cs
@@ -0,0 +1,49 @@
+﻿using Silmoon.Security.Util;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Silmoon.Security.Api
+{
+    public class HttpRequestApiValidation
+    {
+        public static (string data, string sign) GetHttpRequestCollectionSign(NameValueCollection nameValueCollectionWithoutKey, string keyName, string key, params string[] excludedParams)
+        {
+            List<string> array = new List<string>();
+            for (int i = 0; i < nameValueCollectionWithoutKey.Count; i++)
+            {
+                if (excludedParams.Contains(nameValueCollectionWithoutKey.GetKey(i))) continue;
+
+                array.Add(nameValueCollectionWithoutKey.GetKey(i) + "=" + nameValueCollectionWithoutKey[i]);
+            }
+            array.Sort();
+
+            string data = default;
+
+            foreach (var item in array)
+                data += item + "&";
+
+            data += keyName + "=" + key;
+
+            var sign = HashHelper.Get32MD5(data);
+
+            return (data, sign);
+        }
+        public static NameValueCollection MergeHttpRequestCollection(HttpRequestBase request)
+        {
+            NameValueCollection collection = request.QueryString;
+            NameValueCollection collection2 = request.Form;
+            foreach (var item in collection2.AllKeys)
+            {
+                if (collection.AllKeys.Contains(item)) continue;
+                else collection.Add(item, collection2[item]);
+            }
+            return collection;
+        }
+    }
+}
---
+++ SilmoonClassLib/Security/Util/DictionarySort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Silmoon.Security.Util
+{
+    public class DictionarySort : System.Collections.IComparer
+    {
+        public int Compare(object oLeft, object oRight)
+        {
+            string sLeft = oLeft as string;
+            string sRight = oRight as string;
+            int iLeftLength = sLeft.Length;
+            int iRightLength = sRight.Length;
+            int index = 0;
+            while (index < iLeftLength && index < iRightLength)
+            {
+                if (sLeft[index] < sRight[index])
+                    return -1;
+                else if (sLeft[index] > sRight[index])
+                    return 1;
+                else
+                    index++;
+            }
+            return iLeftLength - iRightLength;
+
+        }
+    }
+}
