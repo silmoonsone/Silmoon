e89bcf08e9338f3a83c244e5f32994cc51538891
WEIBIN SONG <sone@silmoon.com>
2020-09-13 20:12:45 +0000

    git-svn-id: https://svn.silmoon.com:8443/svn/SilmoonClassLib@150 1573403e-aac7-6b4a-8685-2b6d39f8d130

--- Silmoon.Data/SqlServer/SqlHelper.cs
+++ Silmoon.Data/SqlServer/SqlHelper.cs
@@ -12,12 +12,12 @@ namespace Silmoon.Data.SqlServer
 {
     public class SqlHelper
     {
-        public static (T, NameObjectCollection<object>) MakeObject<T>(SqlDataReader reader, bool closeReader = true) where T : new()
+        public static (T Result, NameObjectCollection<object> DataCollection) MakeObject<T>(SqlDataReader reader, bool closeReader = true) where T : new()
         {
             T obj = new T();
             return MakeObject(reader, obj, closeReader);
         }
-        public static (T, NameObjectCollection<object>) MakeObject<T>(SqlDataReader reader, T obj, bool closeReader = true) where T : new()
+        public static (T Result, NameObjectCollection<object> DataCollection) MakeObject<T>(SqlDataReader reader, T obj, bool closeReader = true) where T : new()
         {
             NameObjectCollection<object> data = new NameObjectCollection<object>();
             for (int i = 0; i < reader.FieldCount; i++)
@@ -40,26 +40,26 @@ namespace Silmoon.Data.SqlServer
             if (closeReader) reader.Close();
             return (obj, data);
         }
-        public static (T[], NameObjectCollection<object>[]) MakeObjects<T>(SqlDataReader reader) where T : new()
+        public static (T[] Results, NameObjectCollection<object>[] DataCollections) MakeObjects<T>(SqlDataReader reader) where T : new()
         {
             List<T> result = new List<T>();
             List<NameObjectCollection<object>> data = new List<NameObjectCollection<object>>();
             while (reader.Read())
             {
                 var r = MakeObject<T>(reader, false);
-                result.Add(r.Item1);
-                data.Add(r.Item2);
+                result.Add(r.Result);
+                data.Add(r.DataCollection);
             }
             reader.Close();
             return (result.ToArray(), data.ToArray());
         }
 
-        public static (T, NameObjectCollection<object>) MakeObject<T>(DataRow row) where T : new()
+        public static (T Result, NameObjectCollection<object> DataCollection) MakeObject<T>(DataRow row) where T : new()
         {
             T obj = new T();
             return MakeObject(row, obj);
         }
-        public static (T, NameObjectCollection<object>) MakeObject<T>(DataRow row, T obj) where T : new()
+        public static (T Result, NameObjectCollection<object> DataCollection) MakeObject<T>(DataRow row, T obj) where T : new()
         {
             NameObjectCollection<object> data = new NameObjectCollection<object>();
             for (int i = 0; i < row.Table.Columns.Count; i++)
@@ -81,7 +81,7 @@ namespace Silmoon.Data.SqlServer
 
             return (obj, data);
         }
-        public static (T[], NameObjectCollection<object>[]) MakeObjects<T>(DataTable dt) where T : new()
+        public static (T[] Results, NameObjectCollection<object>[] DataCollections) MakeObjects<T>(DataTable dt) where T : new()
         {
             T[] result = new T[dt.Rows.Count];
             NameObjectCollection<object>[] data = new NameObjectCollection<object>[dt.Rows.Count];
@@ -89,8 +89,8 @@ namespace Silmoon.Data.SqlServer
             for (int i = 0; i < result.Length; i++)
             {
                 var r = MakeObject<T>(dt.Rows[i]);
-                result[i] = r.Item1;
-                data[i] = r.Item2;
+                result[i] = r.Result;
+                data[i] = r.DataCollection;
             }
 
             return (result, data);
