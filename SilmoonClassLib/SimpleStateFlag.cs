46c46edb0194a660ee6c71d2575dbd2711e024f6
WEIBIN SONG <sone@silmoon.com>
2020-10-06 19:20:58 +0000

    git-svn-id: https://svn.silmoon.com:8443/svn/SilmoonClassLib@157 1573403e-aac7-6b4a-8685-2b6d39f8d130

--- SilmoonClassLib/SimpleStateFlag.cs
+++ SilmoonClassLib/SimpleStateFlag.cs
@@ -16,23 +16,23 @@ namespace Silmoon
         {
 
         }
-        private SimpleStateFlag(bool success, int stateCode, string message)
+        public SimpleStateFlag(bool success, int stateCode, string message)
         {
             Success = success;
             StateCode = stateCode;
             Message = message;
         }
-        private SimpleStateFlag(bool success, int stateCode)
+        public SimpleStateFlag(bool success, int stateCode)
         {
             Success = success;
             StateCode = stateCode;
         }
-        private SimpleStateFlag(bool success, string message)
+        public SimpleStateFlag(bool success, string message)
         {
             Success = success;
             Message = message;
         }
-        private SimpleStateFlag(bool success)
+        public SimpleStateFlag(bool success)
         {
             Success = success;
         }
@@ -52,10 +52,95 @@ namespace Silmoon
         {
             return new SimpleStateFlag(success, stateCode, message);
         }
+        public static SimpleStateFlag Create(bool success, object data)
+        {
+            return new SimpleStateFlag(success) { Data = data };
+        }
+        public static SimpleStateFlag Create(bool success, string message, object data)
+        {
+            return new SimpleStateFlag(success, message) { Data = data };
+        }
+        public static SimpleStateFlag Create(bool success, int stateCode, object data)
+        {
+            return new SimpleStateFlag(success, stateCode) { Data = data };
+        }
+        public static SimpleStateFlag Create(bool success, int stateCode, string message, object data)
+        {
+            return new SimpleStateFlag(success, stateCode, message) { Data = data };
+        }
         public SimpleStateFlag AppendData(object data)
         {
             Data = data;
             return this;
         }
     }
+    public class SimpleStateFlag<T>
+    {
+        public int StateCode { get; set; } = 0;
+        public string Message { get; set; } = "";
+        public bool Success { get; set; } = false;
+        public T Data { get; set; }
+        public SimpleStateFlag()
+        {
+
+        }
+        public SimpleStateFlag(bool success, int stateCode, string message)
+        {
+            Success = success;
+            StateCode = stateCode;
+            Message = message;
+        }
+        public SimpleStateFlag(bool success, int stateCode)
+        {
+            Success = success;
+            StateCode = stateCode;
+        }
+        public SimpleStateFlag(bool success, string message)
+        {
+            Success = success;
+            Message = message;
+        }
+        public SimpleStateFlag(bool success)
+        {
+            Success = success;
+        }
+        public static SimpleStateFlag<T> Create(bool success)
+        {
+            return new SimpleStateFlag<T>(success);
+        }
+        public static SimpleStateFlag<T> Create(bool success, string message)
+        {
+            return new SimpleStateFlag<T>(success, message);
+        }
+        public static SimpleStateFlag<T> Create(bool success, int stateCode)
+        {
+            return new SimpleStateFlag<T>(success, stateCode);
+        }
+        public static SimpleStateFlag<T> Create(bool success, int stateCode, string message)
+        {
+            return new SimpleStateFlag<T>(success, stateCode, message);
+        }
+        public static SimpleStateFlag<T> Create(bool success, T data)
+        {
+            return new SimpleStateFlag<T>(success) { Data = data };
+        }
+        public static SimpleStateFlag<T> Create(bool success, string message, T data)
+        {
+            return new SimpleStateFlag<T>(success, message) { Data = data };
+        }
+        public static SimpleStateFlag<T> Create(bool success, int stateCode, T data)
+        {
+            return new SimpleStateFlag<T>(success, stateCode) { Data = data };
+        }
+        public static SimpleStateFlag<T> Create<T>(bool success, int stateCode, string message, T data)
+        {
+            return new SimpleStateFlag<T>(success, stateCode, message) { Data = data };
+        }
+        public SimpleStateFlag<T> AppendData(T data)
+        {
+            Data = data;
+            return this;
+        }
+    }
+
 }
