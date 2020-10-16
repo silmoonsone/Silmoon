42a077784a3a4586b37adb3c85a9c95c40c21bcd
WEIBIN SONG <sone@silmoon.com>
2020-10-16 15:00:11 +0000

    git-svn-id: https://svn.silmoon.com:8443/svn/SilmoonClassLib@163 1573403e-aac7-6b4a-8685-2b6d39f8d130

--- SilmoonClassLib/IO/FileHelper.cs
+++ SilmoonClassLib/IO/FileHelper.cs
@@ -24,22 +24,39 @@ namespace Silmoon.IO
                 if (!Directory.Exists(dir))
                 {
                     Directory.CreateDirectory(dir);
-                    Console.WriteLine(dir);
                 }
             }
         }
-        public static void ClearEmptyDirectory(string DirectoryPath)
+        public static void ClearEmptyDirectory(string DirectoryPath, string KeepPath)
         {
+            KeepPath += Path.DirectorySeparatorChar;
+            KeepPath = Path.GetDirectoryName(KeepPath);
+            KeepPath += Path.DirectorySeparatorChar;
+
             string[] dirs = DirectoryPath.Split(Path.DirectorySeparatorChar);
             string dir = default;
+            List<string> dirs2 = new List<string>();
 
             foreach (var item in dirs)
             {
                 dir += item + Path.DirectorySeparatorChar;
-                if (!Directory.Exists(dir))
+                dirs2.Add(dir);
+            }
+
+            dirs2.Reverse();
+
+            foreach (var item in dirs2)
+            {
+                if (item == KeepPath) break;
+                if (Directory.Exists(item))
                 {
-                    Directory.CreateDirectory(dir);
-                    Console.WriteLine(dir);
+                    var files = Directory.GetFiles(item);
+                    dirs = Directory.GetDirectories(item);
+
+                    if (files.Length == 0 && dirs.Length == 0)
+                    {
+                        Directory.Delete(item);
+                    }
                 }
             }
         }
