ef03d17d9716a2a3f4741605ea8df9bc678dfb58
WEIBIN SONG <sone@silmoon.com>
2020-11-08 19:20:31 +0000

    git-svn-id: https://svn.silmoon.com:8443/svn/SilmoonClassLib@171 1573403e-aac7-6b4a-8685-2b6d39f8d130

---
+++ Silmoon.Web/Mvc/ViewHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace Silmoon.Web.Mvc
+{
+    public class ViewHelper
+    {
+        public static IEnumerable<SelectListItem> GetSelectListItems(string[] values, string value)
+        {
+            List<SelectListItem> result = new List<SelectListItem>();
+
+            if (!values.Contains(value))
+            {
+                result.Add(new SelectListItem() { Text = value, Value = value, Selected = true });
+            }
+
+            foreach (var item in values)
+            {
+                result.Add(new SelectListItem() { Text = item, Value = item, Selected = item == value });
+            }
+
+            return result;
+        }
+        public static IEnumerable<SelectListItem> GetSelectListItems(Enum value)
+        {
+            return EnumHelper.GetSelectList(value.GetType(), value);
+        }
+    }
+}
