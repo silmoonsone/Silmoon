b29be08d2f58090ac3f6dbd3be22a545e03546d8
WEIBIN SONG <sone@silmoon.com>
2020-11-17 19:40:42 +0000

    git-svn-id: https://svn.silmoon.com:8443/svn/SilmoonClassLib@177 1573403e-aac7-6b4a-8685-2b6d39f8d130

--- Silmoon.Web/Mvc/ViewHelper.cs
+++ Silmoon.Web/Mvc/ViewHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Silmoon.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,7 @@ using System.Web.Mvc.Html;
 
 namespace Silmoon.Web.Mvc
 {
-    public class ViewHelper
+    public static class ViewHelper
     {
         public static IEnumerable<SelectListItem> GetSelectListItems(string[] values, string value)
         {
@@ -26,9 +27,92 @@ namespace Silmoon.Web.Mvc
 
             return result;
         }
+        public static IEnumerable<SelectListItem> GetSelectListItems(IName[] values, IName value)
+        {
+            List<SelectListItem> result = new List<SelectListItem>();
+
+            if (!values.Contains(value))
+            {
+                result.Add(new SelectListItem() { Text = value.Name, Value = value.Name, Selected = true });
+            }
+
+            foreach (var item in values)
+            {
+                result.Add(new SelectListItem() { Text = value.Name, Value = value.Name, Selected = item.Name == value.Name });
+            }
+
+            return result;
+        }
+        public static IEnumerable<SelectListItem> GetSelectListItems(IName[] values, string value)
+        {
+            List<SelectListItem> result = new List<SelectListItem>();
+
+            if (!values.Contains(value))
+            {
+                result.Add(new SelectListItem() { Text = value, Value = value, Selected = true });
+            }
+
+            foreach (var item in values)
+            {
+                result.Add(new SelectListItem() { Text = value, Value = value, Selected = item.Name == value });
+            }
+
+            return result;
+        }
+        public static IEnumerable<SelectListItem> GetSelectListItems(INameValue[] values, INameValue value)
+        {
+            List<SelectListItem> result = new List<SelectListItem>();
+
+            if (!values.Contains(value))
+            {
+                result.Add(new SelectListItem() { Text = value.Name, Value = value.Value, Selected = true });
+            }
+
+            foreach (var item in values)
+            {
+                result.Add(new SelectListItem() { Text = value.Name, Value = value.Value, Selected = item.Value == value.Value });
+            }
+
+            return result;
+        }
         public static IEnumerable<SelectListItem> GetSelectListItems(Enum value)
         {
             return EnumHelper.GetSelectList(value.GetType(), value);
         }
+
+        public static bool Contains(this IName[] names, IName name)
+        {
+            foreach (var item in names)
+            {
+                if (item.Name == name.Name)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool Contains(this IName[] names, string name)
+        {
+            foreach (var item in names)
+            {
+                if (item.Name == name)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool Contains(this INameValue[] names, INameValue name)
+        {
+            foreach (var item in names)
+            {
+                if (item.Name == name.Name)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
     }
 }
