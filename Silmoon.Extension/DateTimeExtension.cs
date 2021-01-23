7148aee6e6fe03294c97f4dd77f3ef0f938c1e72
WEIBIN SONG <sone@silmoon.com>
2021-01-23 03:30:40 +0000

    git-svn-id: https://svn.silmoon.com:8443/svn/SilmoonClassLib@204 1573403e-aac7-6b4a-8685-2b6d39f8d130

--- Silmoon.Extension/DateTimeExtension.cs
+++ Silmoon.Extension/DateTimeExtension.cs
@@ -7,9 +7,13 @@ namespace Silmoon.Extension
 {
     public static class DateTimeExtension
     {
-        public static string ToUnixStyleTimeStamp(this DateTime datetime)
+        public static long ToUnixStyleTimeStamp(this DateTime datetime)
         {
-            return SpecialConverter.GET_UNIX_TIMESTAMP(datetime).ToString();
+            return SpecialConverter.GET_UNIX_TIMESTAMP(datetime);
+        }
+        public static string ToChineseFormat(this DateTime dateTime)
+        {
+            return dateTime.ToString("yyyy-MM-dd HH:mm:ss");
         }
     }
 }
