12dc9b92de2b2c6d1528ef88965ee8dfafc097ce
WEIBIN SONG <sone@silmoon.com>
2021-03-06 22:16:01 +0000

    git-svn-id: https://svn.silmoon.com:8443/svn/SilmoonClassLib@215 1573403e-aac7-6b4a-8685-2b6d39f8d130

---
+++ Silmoon.Extension/DecimalExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Silmoon.Extension
+{
+    public static class DecimalExtension
+    {
+        public static string ToUSD(this decimal value)
+        {
+            return value.ToString("C", new CultureInfo("en-US"));
+        }
+        public static string ToRMB(this decimal value)
+        {
+            return value.ToString("C", new CultureInfo("zh-CN"));
+        }
+    }
+}
