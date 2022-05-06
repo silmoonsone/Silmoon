b36bfb83191218767a4e364156b6421ec86121f3
WEIBIN SONG <sone@silmoon.com>
2022-05-06 06:06:37 +0000

    git-svn-id: https://svn.silmoon.com:8443/svn/SilmoonClassLib@329 1573403e-aac7-6b4a-8685-2b6d39f8d130

--- Silmoon.AspNetCore/Extensions/HttpRequestExtension.cs
+++ Silmoon.AspNetCore/Extensions/HttpRequestExtension.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.Net.Http.Headers;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Silmoon.Extension;
@@ -18,39 +19,10 @@ namespace Silmoon.AspNetCore.Extensions
     public static class HttpRequestExtension
     {
         public static string Url { get; set; }
-        public static string GetRawUrl(this HttpRequest httpRequest)
-        {
-            return httpRequest.GetEncodedUrl();
-        }
-        public static bool IsAjaxRequest(this HttpRequest httpRequest)
-        {
-            if (httpRequest == null)
-            {
-                throw new ArgumentNullException("request");
-            }
-
-            if (!(httpRequest.Query["X-Requested-With"] == "XMLHttpRequest"))
-            {
-                if (httpRequest.Headers != null)
-                {
-                    return httpRequest.Headers["X-Requested-With"] == "XMLHttpRequest";
-                }
-
-                return false;
-            }
-
-            return true;
-        }
-        public static string UserAgent(this HttpRequest httpRequest)
-        {
-            return httpRequest.Headers["User-Agent"];
-        }
-        public static JObject ReadToJson(this HttpRequest request)
-        {
-            var requestBody = request.Body.MakeToString();
-            return JsonConvert.DeserializeObject<JObject>(requestBody);
-
-        }
+        public static string GetRawUrl(this HttpRequest httpRequest) => httpRequest.GetEncodedUrl();
+        public static bool IsAjaxRequest(this HttpRequest httpRequest) => string.Equals(httpRequest.Query[HeaderNames.XRequestedWith], "XMLHttpRequest", StringComparison.Ordinal) || string.Equals(httpRequest.Headers.XRequestedWith, "XMLHttpRequest", StringComparison.Ordinal);
+        public static string UserAgent(this HttpRequest httpRequest) => httpRequest.Headers["User-Agent"];
+        public static JObject ReadToJson(this HttpRequest request) => JsonConvert.DeserializeObject<JObject>(request.Body.MakeToString());
         public static XmlDocument ReadToXml(this HttpRequest request)
         {
             var requestBody = request.Body.MakeToString();
@@ -61,9 +33,7 @@ namespace Silmoon.AspNetCore.Extensions
         public static bool IsMobileDevice(this HttpRequest request)
         {
             string[] mobileAgents = { "iphone", "android", "phone", "mobile", "wap", "netfront", "java", "opera mobi", "opera mini", "ucweb", "windows ce", "symbian", "series", "webos", "sony", "blackberry", "dopod", "nokia", "samsung", "palmsource", "xda", "pieplus", "meizu", "midp", "cldc", "motorola", "foma", "docomo", "up.browser", "up.link", "blazer", "helio", "hosin", "huawei", "novarra", "coolpad", "webos", "techfaith", "palmsource", "alcatel", "amoi", "ktouch", "nexian", "ericsson", "philips", "sagem", "wellcom", "bunjalloo", "maui", "smartphone", "iemobile", "spice", "bird", "zte-", "longcos", "pantech", "gionee", "portalmmm", "jig browser", "hiptop", "benq", "haier", "^lct", "320x320", "240x320", "176x220", "w3c ", "acs-", "alav", "alca", "amoi", "audi", "avan", "benq", "bird", "blac", "blaz", "brew", "cell", "cldc", "cmd-", "dang", "doco", "eric", "hipt", "inno", "ipaq", "java", "jigs", "kddi", "keji", "leno", "lg-c", "lg-d", "lg-g", "lge-", "maui", "maxo", "midp", "mits", "mmef", "mobi", "mot-", "moto", "mwbp", "nec-", "newt", "noki", "oper", "palm", "pana", "pant", "phil", "play", "port", "prox", "qwap", "sage", "sams", "sany", "sch-", "sec-", "send", "seri", "sgh-", "shar", "sie-", "siem", "smal", "smar", "sony", "sph-", "symb", "t-mo", "teli", "tim-", "tosh", "tsm-", "upg1", "upsi", "vk-v", "voda", "wap-", "wapa", "wapi", "wapp", "wapr", "webc", "winw", "winw", "xda", "xda-", "googlebot-mobile" };
-
             bool isMoblie = false;
-
             string userAgent = request.UserAgent().ToLower();
 
             //排除 Windows 桌面系统或苹果桌面系统 
@@ -78,33 +48,26 @@ namespace Silmoon.AspNetCore.Extensions
                     }
                 }
             }
-
             return isMoblie;
         }
         public static NameValueCollection GetQueryStringNameValues(this HttpRequest request)
         {
             var queries = request.Query;
-
             NameValueCollection nameValueCollection = new NameValueCollection();
-
             foreach (var item in queries)
             {
                 nameValueCollection.Add(item.Key, item.Value);
             }
-
             return nameValueCollection;
         }
         public static NameValueCollection GetFormNameValues(this HttpRequest request)
         {
             var forms = request.Form;
-
             NameValueCollection nameValueCollection = new NameValueCollection();
-
             foreach (var item in forms)
             {
                 nameValueCollection.Add(item.Key, item.Value);
             }
-
             return nameValueCollection;
         }
         public static async Task<string> GetBodyString(this HttpRequest request, Encoding encoding = null)
