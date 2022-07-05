b015c2183c19beb64d072fc305226f953872355f
WEIBIN SONG <sone@silmoon.com>
2022-07-05 22:06:24 +0000

    git-svn-id: https://svn.silmoon.com:8443/svn/SilmoonClassLib@345 1573403e-aac7-6b4a-8685-2b6d39f8d130

---
+++ Silmoon.Extension/BigIntegerHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Silmoon.Extension
+{
+    public static class BigIntegerHelper
+    {
+        public static BigInteger[] ParseArray(string[] strings)
+        {
+            List<BigInteger> bigIntegers = new List<BigInteger>();
+            foreach (var item in strings)
+            {
+                bigIntegers.Add(BigInteger.Parse(item));
+            }
+            return bigIntegers.ToArray();
+        }
+    }
+}
