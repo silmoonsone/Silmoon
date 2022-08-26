7eb829207c6d1b6b2fabfffd3424301a46219699
WEIBIN SONG <sone@silmoon.com>
2022-08-26 04:46:32 +0000

    git-svn-id: https://svn.silmoon.com:8443/svn/SilmoonClassLib@352 1573403e-aac7-6b4a-8685-2b6d39f8d130

--- Silmoon/Runtime/Collections/DictionaryEx.cs
+++ Silmoon/Runtime/Collections/DictionaryEx.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
 
 namespace Silmoon.Runtime.Collections
 {
-    public class DictionaryEx<TKey, TValue> : Dictionary<TKey, TValue>
+    [Serializable]
+    public class DictionaryEx<TKey, TValue> : Dictionary<TKey, TValue>, ISerializable
     {
+        public DictionaryEx()
+        {
+
+        }
+        public DictionaryEx(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
         public TValue Get(TKey key, TValue defaultValue)
         {
             if (!ContainsKey(key)) return defaultValue;
@@ -15,6 +27,10 @@ namespace Silmoon.Runtime.Collections
         {
             if (!ContainsKey(key)) return default(TValue);
             else return this[key];
+        }
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
         }
     }
 }
