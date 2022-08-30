6380cd1f88548c921e8e0237f6783f47085a315f
WEIBIN SONG <sone@silmoon.com>
2022-08-30 13:24:16 +0000

    git-svn-id: https://svn.silmoon.com:8443/svn/SilmoonClassLib@353 1573403e-aac7-6b4a-8685-2b6d39f8d130

--- Silmoon.Extension/DecimalExtension.cs
+++ Silmoon.Extension/DecimalExtension.cs
@@ -7,17 +7,17 @@ namespace Silmoon.Extension
 {
     public static class DecimalExtension
     {
+        static readonly CultureInfo enUS = new CultureInfo("en-US");
+        static readonly CultureInfo zhCN = new CultureInfo("zh-CN");
         public static string ToUSD(this decimal value)
         {
-            var c = new CultureInfo("en-US");
-            c.NumberFormat.CurrencyNegativePattern = 2;
-            return value.ToString("C", c);
+            enUS.NumberFormat.CurrencyNegativePattern = 2;
+            return value.ToString("C", enUS);
         }
         public static string ToRMB(this decimal value)
         {
-            var c = new CultureInfo("zh-CN");
-            c.NumberFormat.CurrencyNegativePattern = 2;
-            return value.ToString("C", c);
+            zhCN.NumberFormat.CurrencyNegativePattern = 2;
+            return value.ToString("C", zhCN);
         }
     }
 }
