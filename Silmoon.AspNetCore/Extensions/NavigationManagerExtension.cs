f9ee424cf26d82ade041cab327a0b64c8c4b64a6
WEIBIN SONG <sone@silmoon.com>
2022-09-27 07:27:55 +0000

    git-svn-id: https://svn.silmoon.com:8443/svn/SilmoonClassLib@359 1573403e-aac7-6b4a-8685-2b6d39f8d130

---
+++ Silmoon.AspNetCore/Extensions/NavigationManagerExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Silmoon.AspNetCore.Extensions
+{
+    public static class NavigationManagerExtension
+    {
+        public static Dictionary<string, StringValues> GetQueryStringValues(this NavigationManager navigationManager)
+        {
+            return QueryHelpers.ParseQuery(new Uri(navigationManager.Uri).Query);
+        }
+    }
+}
