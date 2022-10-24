a6eff4da3b84d35fbe3c5ca7a3babb136f3d5e0e
WEIBIN SONG <sone@silmoon.com>
2022-10-24 15:19:33 +0000

    git-svn-id: https://svn.silmoon.com:8443/svn/SilmoonClassLib@366 1573403e-aac7-6b4a-8685-2b6d39f8d130

---
+++ Silmoon.AspNetCore/Filters/ParameterSigntureAttribute.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Specialized;
+using System.Threading.Tasks;
+using Silmoon.AspNetCore.Extensions;
+using Silmoon.Extension;
+using Silmoon.Models;
+using Silmoon.Models.Types;
+
+namespace Silmoon.AspNetCore.Filters
+{
+    public class ParameterSigntureAttribute : ActionFilterAttribute
+    {
+        public string KeyFieldName { get; set; }
+        public string Key { get; set; }
+        public string SignFieldName { get; set; }
+        public bool Require { get; set; }
+        public ParameterSigntureAttribute(string KeyFieldName, string Key, string SignFieldName, bool Require) : base()
+        {
+            this.KeyFieldName = KeyFieldName;
+            this.Key = Key;
+            this.SignFieldName = SignFieldName;
+            this.Require = Require;
+        }
+
+
+        public async override Task OnActionExecutionAsync(ActionExecutingContext filterContext, ActionExecutionDelegate next)
+        {
+            if (((string)filterContext.HttpContext.Request.Headers["ApiEncrypt"]).ToBool())
+            {
+                await base.OnActionExecutionAsync(filterContext, next);
+                return;
+            }
+            else
+            {
+                string Sign;
+                NameValueCollection nameValueCollection;
+
+                if (filterContext.HttpContext.Request.Method == "GET")
+                {
+                    Sign = filterContext.HttpContext.Request.Query[SignFieldName];
+                    nameValueCollection = filterContext.HttpContext.Request.GetQueryStringNameValues();
+                }
+                else
+                {
+                    Sign = filterContext.HttpContext.Request.Form[SignFieldName];
+                    nameValueCollection = filterContext.HttpContext.Request.GetFormNameValues();
+                }
+
+                if (Require || !Sign.IsNullOrEmpty())
+                {
+                    var sign = nameValueCollection.GetSign(KeyFieldName, Key, SignFieldName);
+                    if (Sign != sign)
+                        filterContext.Result = new ContentResult() { Content = ApiResult<string>.CreateToJsonString(ResultState.Fail, null, "Sign error."), ContentType = "application/json" };
+                    else await base.OnActionExecutionAsync(filterContext, next);
+                }
+                else await base.OnActionExecutionAsync(filterContext, next);
+            }
+        }
+    }
+}
---
+++ Silmoon.Models/Types/ResultState.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Silmoon.Models.Types
+{
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum ResultState
+    {
+        DataFormatError = -2,
+        Unknown = 0,
+        Success = 1,
+        Info = 2,
+        Fail = 3,
+        Exception = 5,
+    }
+}
