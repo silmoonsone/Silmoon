de3df691573efe00be6a4d7da4213f25696a0662
WEIBIN SONG <sone@silmoon.com>
2022-11-09 22:45:20 +0000

    git-svn-id: https://svn.silmoon.com:8443/svn/SilmoonClassLib@370 1573403e-aac7-6b4a-8685-2b6d39f8d130

--- Silmoon.AspNetCore/Binders/BigIntegerBinderProvider.cs
+++ Silmoon.AspNetCore/Binders/BigIntegerBinderProvider.cs
@@ -9,7 +9,7 @@ namespace Silmoon.AspNetCore.Binders
 {
     public class BigIntegerBinderProvider : IModelBinderProvider
     {
-        public IModelBinder? GetBinder(ModelBinderProviderContext context)
+        public IModelBinder GetBinder(ModelBinderProviderContext context)
         {
             if (context.Metadata.ModelType == typeof(BigInteger))
             {
