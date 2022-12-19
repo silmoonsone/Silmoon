7f600b5d5984aab821dce23adcc9d33949d3773f
WEIBIN SONG <sone@silmoon.com>
2022-12-19 06:20:03 +0000

    git-svn-id: https://svn.silmoon.com:8443/svn/SilmoonClassLib@388 1573403e-aac7-6b4a-8685-2b6d39f8d130

--- Silmoon.Extension/ObjectExtension.cs
+++ Silmoon.Extension/ObjectExtension.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -9,6 +10,28 @@ namespace Silmoon.Extension
         public static string ToStringOrNull(this object obj)
         {
             return obj?.ToString();
+        }
+        //convert a object to JObject
+        public static JObject ToJObject(this object obj)
+        {
+            return JObject.FromObject(obj);
+        }
+        //convert a object array to JArray
+        public static JArray ToJArray(this object[] obj)
+        {
+            return JArray.FromObject(obj);
+        }
+        //if a object is array, convert this object to JArray
+        public static JArray ToJArray(this object obj)
+        {
+            if (obj is Array)
+            {
+                return JArray.FromObject(obj);
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 }
