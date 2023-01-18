67374d09a45f0ef847c016a3c7a276f8d9dcd8dc
WEIBIN SONG <sone@silmoon.com>
2023-01-18 00:29:33 +0000

    git-svn-id: https://svn.silmoon.com:8443/svn/SilmoonClassLib@392 1573403e-aac7-6b4a-8685-2b6d39f8d130

--- Silmoon.AspNetCore.Test/Controllers/UserController.cs
+++ Silmoon.AspNetCore.Test/Controllers/UserController.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Silmoon.AspNetCore.Services.Interfaces;
+using Silmoon.AspNetCore.Test.Models;
 
 namespace Silmoon.AspNetCore.Test.Controllers
 {
     public class UserController : Controller
     {
+        ISilmoonUserService<User> SilmoonUserService { get; set; }
+        public UserController(ISilmoonUserService<User> silmoonUserService)
+        {
+            SilmoonUserService = silmoonUserService;
+        }
         public IActionResult Index()
         {
             return View();
