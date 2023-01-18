4266e3d60e4e5a39a3c54ae80237d7f9658feb8a
WEIBIN SONG <sone@silmoon.com>
2023-01-18 17:04:25 +0000

    git-svn-id: https://svn.silmoon.com:8443/svn/SilmoonClassLib@400 1573403e-aac7-6b4a-8685-2b6d39f8d130

--- Silmoon.AspNetCore/Filters/UserTokenSignInAutoRefreshAttribute.cs
+++ Silmoon.AspNetCore/Filters/UserTokenSignInAutoRefreshAttribute.cs
@@ -9,7 +9,7 @@ namespace Silmoon.AspNetCore.Filters
     /// <summary>
     /// 实现使用UserToken登录的时候，在当前请求刷新登录状态，登录标志为UserTokenSignin，UserToken为UserToken。
     /// </summary>
-    public class UserTokenSigninAutoRefreshAttribute1 : ActionFilterAttribute
+    public class UserTokenSignInAutoRefreshAttribute : ActionFilterAttribute
     {
         public async override void OnActionExecuting(ActionExecutingContext filterContext)
         {
