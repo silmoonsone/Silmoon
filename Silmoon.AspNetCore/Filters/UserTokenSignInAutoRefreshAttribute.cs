03f806362d70d4b96e9209f9857a0af2c9a84d4b
WEIBIN SONG <sone@silmoon.com>
2023-04-13 16:20:24 +0800

    更新NuGet包，完善UserTokenSignInAutoRefreshAttribute。

--- Silmoon.AspNetCore/Filters/UserTokenSignInAutoRefreshAttribute.cs
+++ Silmoon.AspNetCore/Filters/UserTokenSignInAutoRefreshAttribute.cs
@@ -3,11 +3,14 @@ using Microsoft.AspNetCore.Mvc;
 using Silmoon.Extension;
 using Silmoon.AspNetCore.Services.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
+using System.Web;
 
 namespace Silmoon.AspNetCore.Filters
 {
     /// <summary>
-    /// 实现使用UserToken登录的时候，在当前请求刷新登录状态，登录标志为UserTokenSignin，UserToken为UserToken。
+    /// 此行为必须在验证过滤器【<see cref="Silmoon.AspNetCore.Filters.RequireSessionAttribute{TUser}"/>】之后使用！<br />
+    /// 实现使用<c>UserToken</c>登录的时候，如果是没有登录并且有<c>UserTokenSignIn</c>、<c>UserToken</c>两个参数，就会停止接下来的控制器处理，并且重新刷新一次页面。<br />
+    /// 重新刷新后会删除<c>UserTokenSignIn</c>、<c>UserToken</c>两个请求参数。<br />
     /// </summary>
     public class UserTokenSignInAutoRefreshAttribute : ActionFilterAttribute
     {
@@ -15,7 +18,16 @@ namespace Silmoon.AspNetCore.Filters
         {
             var silmoonAuthService = filterContext.HttpContext.RequestServices.GetService<ISilmoonAuthService>();
             if (!await silmoonAuthService.IsSignIn() && filterContext.HttpContext.Request.Query["UserTokenSignIn"].ToString().ToBool() && !filterContext.HttpContext.Request.Query["UserToken"].ToString().IsNullOrEmpty())
-                filterContext.Result = new RedirectResult((string)filterContext.HttpContext.Request.RouteValues["action"] + filterContext.HttpContext.Request.QueryString);
+            {
+                var queryString = filterContext.HttpContext.Request.QueryString.ToString();
+                var query = HttpUtility.ParseQueryString(queryString);
+                query.Remove("UserTokenSignIn");
+                query.Remove("UserToken");
+
+                queryString = query.ToString();
+
+                filterContext.Result = new RedirectResult((string)filterContext.HttpContext.Request.RouteValues["action"] + queryString);
+            }
 
             base.OnActionExecuting(filterContext);
         }
