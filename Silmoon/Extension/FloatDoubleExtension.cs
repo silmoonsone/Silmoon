4bb26929e7188ce0852ff4db7aafa158be902ce3
WEIBIN SONG <sone@silmoon.com>
2023-10-10 19:14:25 +0800

    增加原生小数类型的到decimal的扩展方法（ToDecimal），增加了一个不管结果如何只管执行的帮助方法，更新NuGet包。

---
+++ Silmoon/Extension/FloatDoubleExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Silmoon.Extension
+{
+    public static class FloatDoubleExtension
+    {
+        public static decimal ToDecimal(this double value) => Convert.ToDecimal(value);
+        public static decimal ToDecimal(this float value) => Convert.ToDecimal(value);
+    }
+}
--- Silmoon/Runtime/CSharpHelper.cs
+++ Silmoon/Runtime/CSharpHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Silmoon.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,15 +7,90 @@ namespace Silmoon.Runtime
 {
     public class CSharpHelper
     {
-        [Obsolete]
-        public static string[] NamesOf(params object[] objects)
+        public static void TryRun(Action action)
         {
-            string[] result = new string[objects.Length];
-            for (int i = 0; i < objects.Length; i++)
+            try
             {
-                //result[i] = nameof(objects[i]);
+                action();
             }
-            return result;
+            catch { }
+        }
+        public static void TryRun<T>(Action<T> action, T t)
+        {
+            try
+            {
+                action(t);
+            }
+            catch { }
+        }
+        public static void TryRun<T1, T2>(Action<T1, T2> action, T1 t1, T2 t2)
+        {
+            try
+            {
+                action(t1, t2);
+            }
+            catch { }
+        }
+        public static void TryRun<T1, T2, T3>(Action<T1, T2, T3> action, T1 t1, T2 t2, T3 t3)
+        {
+            try
+            {
+                action(t1, t2, t3);
+            }
+            catch { }
+        }
+        public static void TryRun<T1, T2, T3, T4>(Action<T1, T2, T3, T4> action, T1 t1, T2 t2, T3 t3, T4 t4)
+        {
+            try
+            {
+                action(t1, t2, t3, t4);
+            }
+            catch { }
+        }
+        public static StateSet<bool, ReturnT> TryRun<ReturnT>(Func<ReturnT> func)
+        {
+            try
+            {
+                return StateSet<bool, ReturnT>.Create(true, func());
+            }
+            catch { }
+            return StateSet<bool, ReturnT>.Create(false, default);
+        }
+        public static StateSet<bool, ReturnT> TryRun<T, ReturnT>(Func<T, ReturnT> func, T t)
+        {
+            try
+            {
+                return StateSet<bool, ReturnT>.Create(true, func(t));
+            }
+            catch { }
+            return StateSet<bool, ReturnT>.Create(false, default);
+        }
+        public static StateSet<bool, ReturnT> TryRun<T1, T2, ReturnT>(Func<T1, T2, ReturnT> func, T1 t1, T2 t2)
+        {
+            try
+            {
+                return StateSet<bool, ReturnT>.Create(true, func(t1, t2));
+            }
+            catch { }
+            return StateSet<bool, ReturnT>.Create(false, default);
+        }
+        public static StateSet<bool, ReturnT> TryRun<T1, T2, T3, ReturnT>(Func<T1, T2, T3, ReturnT> func, T1 t1, T2 t2, T3 t3)
+        {
+            try
+            {
+                return StateSet<bool, ReturnT>.Create(true, func(t1, t2, t3));
+            }
+            catch { }
+            return StateSet<bool, ReturnT>.Create(false, default);
+        }
+        public static StateSet<bool, ReturnT> TryRun<T1, T2, T3, T4, ReturnT>(Func<T1, T2, T3, T4, ReturnT> func, T1 t1, T2 t2, T3 t3, T4 t4)
+        {
+            try
+            {
+                return StateSet<bool, ReturnT>.Create(true, func(t1, t2, t3, t4));
+            }
+            catch { }
+            return StateSet<bool, ReturnT>.Create(false, default);
         }
     }
 }
