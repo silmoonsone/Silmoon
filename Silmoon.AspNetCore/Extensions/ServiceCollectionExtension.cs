beb170267d013428e0d62b8d5d30efc82d64ceca
WEIBIN SONG <sone@silmoon.com>
2023-11-08 14:01:03 +0800

    AddSilmoonDevApp添加服务模式改成瞬时模式。

--- Silmoon.AspNetCore/Extensions/ServiceCollectionExtension.cs
+++ Silmoon.AspNetCore/Extensions/ServiceCollectionExtension.cs
@@ -15,7 +15,7 @@ namespace Silmoon.AspNetCore.Extensions
             if (services == null) throw new ArgumentNullException(nameof(services));
 
             services.Configure<SilmoonAppDevServiceOptions>(o => o.KeyCacheSecoundTimeout = 3600);
-            services.AddSingleton<ISilmoonDevAppService, TSilmoonDevAppService>();
+            services.AddTransient<ISilmoonDevAppService, TSilmoonDevAppService>();
         }
 
         public static void AddSilmoonDevApp<TSilmoonDevAppService>(this IServiceCollection services, Action<SilmoonAppDevServiceOptions> configure) where TSilmoonDevAppService : SilmoonDevAppService
@@ -24,7 +24,7 @@ namespace Silmoon.AspNetCore.Extensions
             if (configure == null) throw new ArgumentNullException(nameof(configure));
 
             services.Configure(configure);
-            services.AddSingleton<ISilmoonDevAppService, TSilmoonDevAppService>();
+            services.AddTransient<ISilmoonDevAppService, TSilmoonDevAppService>();
         }
 
         public static void AddSilmoonAuth<TSilmoonAuthService>(this IServiceCollection services) where TSilmoonAuthService : class, ISilmoonAuthService
