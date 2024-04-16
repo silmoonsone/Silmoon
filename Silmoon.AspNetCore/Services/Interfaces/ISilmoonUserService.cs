e6108f13e117740590ccecfb1a5f59e235be314c
WEIBIN SONG <sone@silmoon.com>
2024-04-16 20:50:27 +0800

    针对ISilmoonAuthService接口增加了几个用于登录（SignIn）的重载。

--- Silmoon.AspNetCore/Services/Interfaces/ISilmoonUserService.cs
+++ Silmoon.AspNetCore/Services/Interfaces/ISilmoonUserService.cs
@@ -9,6 +9,8 @@ namespace Silmoon.AspNetCore.Services.Interfaces
     {
         Task SignIn<TUser>(TUser User, bool AddEnumRole) where TUser : class, IDefaultUserIdentity;
         Task SignIn<TUser>(TUser User, string NameIdentifier = null) where TUser : class, IDefaultUserIdentity;
+        Task SignIn<TUser>(TUser User, bool AddEnumRole, Enum CustomerRoles, string NameIdentifier = null) where TUser : class, IDefaultUserIdentity;
+        Task SignIn<TUser>(TUser User, bool AddEnumRole, Enum[] CustomerRoles, string NameIdentifier = null) where TUser : class, IDefaultUserIdentity;
         Task SignIn<TUser>(TUser User, bool AddEnumRole, string[] CustomerRoles, string NameIdentifier = null) where TUser : class, IDefaultUserIdentity;
         Task<bool> SignOut();
         Task<TUser> GetUser<TUser>() where TUser : class, IDefaultUserIdentity;
