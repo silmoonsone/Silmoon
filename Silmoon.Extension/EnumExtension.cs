a7662d3a95b7f86d391e02f70b636eccfb902be6
WEIBIN SONG <sone@silmoon.com>
2024-07-15 18:10:29 +0800

    Enum添加ToNameValue系列相关扩展方法。更新Silmoon和Silmoon.Extension包到1.4.1

--- Silmoon.Extension/EnumExtension.cs
+++ Silmoon.Extension/EnumExtension.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Silmoon.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Reflection;
@@ -14,6 +15,39 @@ namespace Silmoon.Extension
             var field = type.GetField(@enum.ToString());
             var obj = (DisplayAttribute)field.GetCustomAttribute(typeof(DisplayAttribute));//得到特性
             return obj?.Name ?? @enum.ToString();
+        }
+
+        public static List<NameValue<T>> ToNameValues<T>() where T : Enum
+        {
+            List<NameValue<T>> results = new List<NameValue<T>>();
+            foreach (T @enum in Enum.GetValues(typeof(T)))
+            {
+                results.Add(new NameValue<T>(@enum.ToString(), @enum));
+            }
+            return results;
+        }
+        public static List<NameValue<T>> ToDisplayNameValues<T>() where T : Enum
+        {
+            List<NameValue<T>> results = new List<NameValue<T>>();
+            foreach (T @enum in Enum.GetValues(typeof(T)))
+            {
+                results.Add(new NameValue<T>(@enum.GetDisplayName(), @enum));
+            }
+            return results;
+        }
+        public static NameValue<T> ToNameValue<T>(this T @enum) where T : Enum
+        {
+            var type = @enum.GetType();
+            var field = type.GetField(@enum.ToString());
+            var obj = (DisplayAttribute)field.GetCustomAttribute(typeof(DisplayAttribute));//得到特性
+            return new NameValue<T>(@enum.ToString(), @enum);
+        }
+        public static NameValue<T> ToDisplayNameValue<T>(this T @enum) where T : Enum
+        {
+            var type = @enum.GetType();
+            var field = type.GetField(@enum.ToString());
+            var obj = (DisplayAttribute)field.GetCustomAttribute(typeof(DisplayAttribute));//得到特性
+            return new NameValue<T>(obj?.Name ?? @enum.GetDisplayName(), @enum);
         }
     }
 }
