0a34acd16711cc9582eec166a9ec094b4c0c6f57
WEIBIN SONG <sone@silmoon.com>
2024-08-09 17:35:56 +0800

    增加ByteArrayExtension2，用于处理Span类型的扩展方法。发布NuGet包Silmoon.Extension 1.4.4。

---
+++ Silmoon.Extension/ByteArrayExtension2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Silmoon.Extension
+{
+    public static class ByteArrayExtension2
+    {
+        public static string ToHexString(this Span<byte> value, bool TrimPerfixZero, bool Add0xPrefix = false)
+        {
+            if (value == null) return null;
+            StringBuilder sb = new StringBuilder(value.Length * 2);
+            foreach (byte b in value)
+            {
+                sb.Append(b.ToString("x2"));
+            }
+            var hexStr = sb.ToString();
+            if (TrimPerfixZero) hexStr = hexStr.TrimStart('0');
+
+            if (Add0xPrefix) return "0x" + hexStr;
+            else return hexStr;
+        }
+        public static string ToHexString(this Span<byte> value) => ToHexString(value, false, false);
+        public static string ToHexString(this ReadOnlySpan<byte> value, bool TrimPerfixZero, bool Add0xPrefix = false)
+        {
+            if (value == null) return null;
+            StringBuilder sb = new StringBuilder(value.Length * 2);
+            foreach (byte b in value)
+            {
+                sb.Append(b.ToString("x2"));
+            }
+            var hexStr = sb.ToString();
+            if (TrimPerfixZero) hexStr = hexStr.TrimStart('0');
+
+            if (Add0xPrefix) return "0x" + hexStr;
+            else return hexStr;
+        }
+        public static string ToHexString(this ReadOnlySpan<byte> value) => ToHexString(value, false, false);
+
+    }
+}
