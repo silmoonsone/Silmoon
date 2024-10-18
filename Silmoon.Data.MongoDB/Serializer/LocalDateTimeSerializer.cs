8d0074f3adcb63dcff400d13a4ace6125c11946a
WEIBIN SONG <sone@silmoon.com>
2024-10-19 01:31:07 +0800

    增加用户MongoDB全局处理时间的LocalDateTimeSerializer类型。发布NuGet包Silmoon.Data.MongoDB 1.6.0-alpha.2。

---
+++ Silmoon.Data.MongoDB/Serializer/LocalDateTimeSerializer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Silmoon.Data.MongoDB.Serializer
+{
+    public class LocalDateTimeSerializer : DateTimeSerializer
+    {
+        public LocalDateTimeSerializer() : base(DateTimeKind.Local) { }
+    }
+}
