51201daed6d5b3a5e44ac81b0e5fc6c7dfa57bc6
WEIBIN SONG <sone@silmoon.com>
2025-04-20 15:17:29 +0800

    重构文件类，删除 VirtualFile 并添加BinaryFileData。更新NuGet包Silmoon 1.8.2。

--- Silmoon/Models/BinaryFileData.cs
+++ Silmoon/Models/BinaryFileData.cs
@@ -1,28 +1,27 @@
 ﻿using Silmoon.Extension;
-using Silmoon.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Silmoon.IO
+namespace Silmoon.Models
 {
-    public class VirtualFile
+    public class BinaryFileData
     {
         public byte[] Content { get; set; }
         public int Length { get; set; }
         public string ContentType { get; set; }
 
-        public VirtualFile()
+        public BinaryFileData()
         {
 
         }
-        public VirtualFile(byte[] content)
+        public BinaryFileData(byte[] content)
         {
             Content = content;
             Length = content.Length;
             ContentType = content.GetMimeInfo()?.Mime;
         }
-        public VirtualFile(byte[] content, string contentType)
+        public BinaryFileData(byte[] content, string contentType)
         {
             Content = content;
             Length = content.Length;
