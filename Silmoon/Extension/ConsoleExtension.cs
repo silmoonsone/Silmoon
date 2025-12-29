f31afe18b056db0492b5de4b3fbe306192459b84
WEIBIN SONG <sone@silmoon.com>
2025-12-29 21:33:28 +0800

    improvement console ansi color.

--- Silmoon/Extension/ConsoleExtension.cs
+++ Silmoon/Extension/ConsoleExtension.cs
@@ -6,6 +6,8 @@ namespace Silmoon.Extension
 {
     public static class ConsoleExtension
     {
+        public static string FgRgb(byte r, byte g, byte b) => $"\x1b[38;2;{r};{g};{b}m";
+        public static string BgRgb(byte r, byte g, byte b) => $"\x1b[48;2;{r};{g};{b}m";
         public static class Foreground
         {
             public const string Black = "\x1b[30m";
@@ -46,7 +48,28 @@ namespace Silmoon.Extension
             public const string BrightCyan = "\x1b[106m";
             public const string White = "\x1b[107m";
         }
+        public static class ConsoleColorRgb
+        {
+            // Dark*
+            public static readonly (byte r, byte g, byte b) DarkBlue = (0, 0, 128);
+            public static readonly (byte r, byte g, byte b) DarkGreen = (0, 128, 0);
+            public static readonly (byte r, byte g, byte b) DarkCyan = (0, 128, 128);
+            public static readonly (byte r, byte g, byte b) DarkRed = (128, 0, 0);
+            public static readonly (byte r, byte g, byte b) DarkMagenta = (128, 0, 128);
+            public static readonly (byte r, byte g, byte b) DarkYellow = (128, 128, 0); // “棕/暗黄”
+            public static readonly (byte r, byte g, byte b) Gray = (192, 192, 192); // 传统“灰”
+            public static readonly (byte r, byte g, byte b) DarkGray = (128, 128, 128);
 
+            // Light*
+            public static readonly (byte r, byte g, byte b) Blue = (0, 0, 255);
+            public static readonly (byte r, byte g, byte b) Green = (0, 255, 0);
+            public static readonly (byte r, byte g, byte b) Cyan = (0, 255, 255);
+            public static readonly (byte r, byte g, byte b) Red = (255, 0, 0);
+            public static readonly (byte r, byte g, byte b) Magenta = (255, 0, 255);
+            public static readonly (byte r, byte g, byte b) Yellow = (255, 255, 0);
+            public static readonly (byte r, byte g, byte b) White = (255, 255, 255);
+            public static readonly (byte r, byte g, byte b) Black = (0, 0, 0);
+        }
 
         // Reset / Clear
         public const string Reset = "\x1b[0m";
@@ -56,6 +79,46 @@ namespace Silmoon.Extension
         public const string ResetBackground = "\x1b[49m";
 
         // Mapping ConsoleColor to ANSI Codes
+        private static readonly Dictionary<ConsoleColor, string> ForegroundRgbColorMap = new Dictionary<ConsoleColor, string>()
+        {
+            { ConsoleColor.Black, FgRgb(0,0,0) },
+            { ConsoleColor.DarkGray, FgRgb(ConsoleColorRgb.DarkGray.r, ConsoleColorRgb.DarkGray.g, ConsoleColorRgb.DarkGray.b) },
+            { ConsoleColor.Gray, FgRgb(ConsoleColorRgb.Gray.r, ConsoleColorRgb.Gray.g, ConsoleColorRgb.Gray.b) },
+            { ConsoleColor.White, FgRgb(ConsoleColorRgb.White.r, ConsoleColorRgb.White.g, ConsoleColorRgb.White.b) },
+            { ConsoleColor.DarkBlue, FgRgb(ConsoleColorRgb.DarkBlue.r, ConsoleColorRgb.DarkBlue.g, ConsoleColorRgb.DarkBlue.b) },
+            { ConsoleColor.DarkGreen, FgRgb(ConsoleColorRgb.DarkGreen.r, ConsoleColorRgb.DarkGreen.g, ConsoleColorRgb.DarkGreen.b) },
+            { ConsoleColor.DarkCyan, FgRgb(ConsoleColorRgb.DarkCyan.r, ConsoleColorRgb.DarkCyan.g, ConsoleColorRgb.DarkCyan.b) },
+            { ConsoleColor.DarkRed, FgRgb(ConsoleColorRgb.DarkRed.r, ConsoleColorRgb.DarkRed.g, ConsoleColorRgb.DarkRed.b) },
+            { ConsoleColor.DarkMagenta, FgRgb(ConsoleColorRgb.DarkMagenta.r, ConsoleColorRgb.DarkMagenta.g, ConsoleColorRgb.DarkMagenta.b) },
+            { ConsoleColor.DarkYellow, FgRgb(ConsoleColorRgb.DarkYellow.r, ConsoleColorRgb.DarkYellow.g, ConsoleColorRgb.DarkYellow.b) },
+            { ConsoleColor.Blue, FgRgb(ConsoleColorRgb.Blue.r, ConsoleColorRgb.Blue.g, ConsoleColorRgb.Blue.b) },
+            { ConsoleColor.Green, FgRgb(ConsoleColorRgb.Green.r, ConsoleColorRgb.Green.g, ConsoleColorRgb.Green.b) },
+            { ConsoleColor.Cyan, FgRgb(ConsoleColorRgb.Cyan.r, ConsoleColorRgb.Cyan.g, ConsoleColorRgb.Cyan.b) },
+            { ConsoleColor.Red, FgRgb(ConsoleColorRgb.Red.r, ConsoleColorRgb.Red.g, ConsoleColorRgb.Red.b) },
+            { ConsoleColor.Magenta, FgRgb(ConsoleColorRgb.Magenta.r, ConsoleColorRgb.Magenta.g, ConsoleColorRgb.Magenta.b) },
+            { ConsoleColor.Yellow, FgRgb(ConsoleColorRgb.Yellow.r, ConsoleColorRgb.Yellow.g, ConsoleColorRgb.Yellow.b) },
+        };
+
+        private static readonly Dictionary<ConsoleColor, string> BackgroundRgbColorMap = new Dictionary<ConsoleColor, string>()
+        {
+            { ConsoleColor.Black, BgRgb(0,0,0) },
+            { ConsoleColor.DarkGray, BgRgb(ConsoleColorRgb.DarkGray.r, ConsoleColorRgb.DarkGray.g, ConsoleColorRgb.DarkGray.b) },
+            { ConsoleColor.Gray, BgRgb(ConsoleColorRgb.Gray.r, ConsoleColorRgb.Gray.g, ConsoleColorRgb.Gray.b) },
+            { ConsoleColor.White, BgRgb(ConsoleColorRgb.White.r, ConsoleColorRgb.White.g, ConsoleColorRgb.White.b) },
+            { ConsoleColor.DarkBlue, BgRgb(ConsoleColorRgb.DarkBlue.r, ConsoleColorRgb.DarkBlue.g, ConsoleColorRgb.DarkBlue.b) },
+            { ConsoleColor.DarkGreen, BgRgb(ConsoleColorRgb.DarkGreen.r, ConsoleColorRgb.DarkGreen.g, ConsoleColorRgb.DarkGreen.b) },
+            { ConsoleColor.DarkCyan, BgRgb(ConsoleColorRgb.DarkCyan.r, ConsoleColorRgb.DarkCyan.g, ConsoleColorRgb.DarkCyan.b) },
+            { ConsoleColor.DarkRed, BgRgb(ConsoleColorRgb.DarkRed.r, ConsoleColorRgb.DarkRed.g, ConsoleColorRgb.DarkRed.b) },
+            { ConsoleColor.DarkMagenta, BgRgb(ConsoleColorRgb.DarkMagenta.r, ConsoleColorRgb.DarkMagenta.g, ConsoleColorRgb.DarkMagenta.b) },
+            { ConsoleColor.DarkYellow, BgRgb(ConsoleColorRgb.DarkYellow.r, ConsoleColorRgb.DarkYellow.g, ConsoleColorRgb.DarkYellow.b) },
+            { ConsoleColor.Blue, BgRgb(ConsoleColorRgb.Blue.r, ConsoleColorRgb.Blue.g, ConsoleColorRgb.Blue.b) },
+            { ConsoleColor.Green, BgRgb(ConsoleColorRgb.Green.r, ConsoleColorRgb.Green.g, ConsoleColorRgb.Green.b) },
+            { ConsoleColor.Cyan, BgRgb(ConsoleColorRgb.Cyan.r, ConsoleColorRgb.Cyan.g, ConsoleColorRgb.Cyan.b) },
+            { ConsoleColor.Red, BgRgb(ConsoleColorRgb.Red.r, ConsoleColorRgb.Red.g, ConsoleColorRgb.Red.b) },
+            { ConsoleColor.Magenta, BgRgb(ConsoleColorRgb.Magenta.r, ConsoleColorRgb.Magenta.g, ConsoleColorRgb.Magenta.b) },
+            { ConsoleColor.Yellow, BgRgb(ConsoleColorRgb.Yellow.r, ConsoleColorRgb.Yellow.g, ConsoleColorRgb.Yellow.b) },
+        };
+
         private static readonly Dictionary<ConsoleColor, string> ForegroundColorMap = new Dictionary<ConsoleColor, string>()
         {
             { ConsoleColor.Black, Foreground.Black },
@@ -101,6 +164,7 @@ namespace Silmoon.Extension
         };
 
 
+
         // 256-color / TrueColor
         public static string Foreground256(int index) => $"\x1b[38;5;{index}m";
         public static string Background256(int index) => $"\x1b[48;5;{index}m";
@@ -112,8 +176,8 @@ namespace Silmoon.Extension
 #if NET10_0_OR_GREATER
         extension(Console)
         {
-            public static string GetAnsiForegroundColorString(ConsoleColor color) => ForegroundColorMap.TryGetValue(color, out var code) ? code : ResetForeground;
-            public static string GetAnsiBackgroundColorString(ConsoleColor color) => BackgroundColorMap.TryGetValue(color, out var code) ? code : ResetBackground;
+            public static string GetAnsiForegroundColorString(ConsoleColor color) => ForegroundRgbColorMap.TryGetValue(color, out var code) ? code : ResetForeground;
+            public static string GetAnsiBackgroundColorString(ConsoleColor color) => BackgroundRgbColorMap.TryGetValue(color, out var code) ? code : ResetBackground;
             public static string GetAnsiColorString(ConsoleColor foregroundColor, ConsoleColor backgroundColor) => string.Concat(GetAnsiForegroundColorString(foregroundColor), GetAnsiBackgroundColorString(backgroundColor));
 
             public static void ResetAnsiColor() => Console.Write(Reset);
@@ -122,8 +186,8 @@ namespace Silmoon.Extension
             public static string WarpStringANSIColor(string text, ConsoleColor? foregroundColor = null, ConsoleColor? backgroundColor = null) => string.Concat(GetAnsiColorString(foregroundColor ?? Console.ForegroundColor, backgroundColor ?? Console.BackgroundColor), text, ResetForeground, ResetBackground);
         }
 #else
-        public static string GetAnsiForegroundColorString(ConsoleColor color) => ForegroundColorMap.TryGetValue(color, out var code) ? code : ResetForeground;
-        public static string GetAnsiBackgroundColorString(ConsoleColor color) => BackgroundColorMap.TryGetValue(color, out var code) ? code : ResetBackground;
+        public static string GetAnsiForegroundColorString(ConsoleColor color) => ForegroundRgbColorMap.TryGetValue(color, out var code) ? code : ResetForeground;
+        public static string GetAnsiBackgroundColorString(ConsoleColor color) => BackgroundRgbColorMap.TryGetValue(color, out var code) ? code : ResetBackground;
         public static string GetAnsiColorString(ConsoleColor foregroundColor, ConsoleColor backgroundColor) => string.Concat(GetAnsiForegroundColorString(foregroundColor), GetAnsiBackgroundColorString(backgroundColor));
 
         public static void ResetAnsiColor() => Console.Write(Reset);
