e72bb440f1519ef26b83806619a928d864b5d1e7
WEIBIN SONG <sone@silmoon.com>
2026-01-10 17:46:30 +0800

    从Silmoon迁移一些网络操作的代码到Silmoon.Net。

---
+++ NetWinFormTesting/MainForm.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace NetWinFormTesting
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(385, 426);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "服务器端";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Location = new Point(403, 12);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(385, 426);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "客户端";
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "MainForm";
+            Text = "MainForm";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+    }
+}
--- NetWinFormTesting/MainForm.cs
+++ NetWinFormTesting/MainForm.cs
@@ -1,8 +1,8 @@
 namespace NetWinFormTesting
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
