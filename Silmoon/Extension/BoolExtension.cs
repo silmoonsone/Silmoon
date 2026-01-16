923ace8c2217c956996cf14ab6b5e76bad32b4de
WEIBIN SONG <sone@silmoon.com>
2026-01-17 05:20:41 +0800

    添加一个bool的静态扩展方法Flip，用于随机一个布尔值。发布NuGet包【Silmoon 2.0.6】、【Silmoon.Extension 2.0.3】、【Silmoon.Net 2.0.1】。

--- Silmoon/Extension/BoolExtension.cs
+++ Silmoon/Extension/BoolExtension.cs
@@ -12,13 +12,7 @@ namespace Silmoon.Extension
         public static StateFlag ToStateFlag(this bool value, string message = null) => StateFlag.Create(value, message);
         public static StateFlag ToStateFlag<T>(this bool value, T data, string message = null) => StateFlag<T>.Create(value, data, message);
         public static T IfElse<T>(this bool value, T trueResult, T falseResult) => value ? trueResult : falseResult;
-        public static T IfElse<T>(this bool? value, T trueResult, T falseResult, T nullResult)
-        {
-            if (value.HasValue)
-                return value.Value ? trueResult : falseResult;
-            else
-                return nullResult;
-        }
+        public static T IfElse<T>(this bool? value, T trueResult, T falseResult, T nullResult) => value.HasValue ? IfElse(value.Value, trueResult, falseResult) : nullResult;
         public static void IfTrue(this bool value, Action action)
         {
             if (value) action();
@@ -32,5 +26,12 @@ namespace Silmoon.Extension
             if (value) trueAction();
             else falseAction();
         }
+
+#if NET10_0_OR_GREATER
+        extension(bool)
+        {
+            public static bool Flip() => Random.Shared.Next(0, 2) == 0;
+        }
+#endif
     }
 }
