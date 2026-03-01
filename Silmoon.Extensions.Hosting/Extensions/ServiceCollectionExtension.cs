2b20233ddb8e0d6da9969e2a6ca5f43fa92e0039
WEIBIN SONG <sone@silmoon.com>
2026-03-02 01:52:36 +0800

    Silmoon.Extensions.Hosting add extensions.

---
+++ Silmoon.Extensions.Hosting/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Silmoon.Extensions.Hosting.Interfaces;
+using Silmoon.Extensions.Hosting.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Silmoon.Extensions.Hosting.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static void AddSilmoonConfigure<TSilmoonConfigureService>(this IServiceCollection services) where TSilmoonConfigureService : class, ISilmoonConfigureService
+        {
+            ArgumentNullException.ThrowIfNull(services);
+
+            services.AddSingleton<ISilmoonConfigureService, TSilmoonConfigureService>();
+        }
+        public static void AddSilmoonConfigure<TSilmoonConfigureService>(this IServiceCollection services, Action<SilmoonConfigureServiceOption> option) where TSilmoonConfigureService : class, ISilmoonConfigureService
+        {
+            ArgumentNullException.ThrowIfNull(services);
+            ArgumentNullException.ThrowIfNull(option);
+
+            services.Configure(option);
+            services.AddSingleton<ISilmoonConfigureService, TSilmoonConfigureService>();
+        }
+        public static void AddSilmoonConfigure(this IServiceCollection services)
+        {
+            ArgumentNullException.ThrowIfNull(services);
+            services.AddSingleton<ISilmoonConfigureService, SilmoonConfigureService>();
+        }
+        public static void AddSilmoonConfigure(this IServiceCollection services, Action<SilmoonConfigureServiceOption> option)
+        {
+            ArgumentNullException.ThrowIfNull(services);
+            ArgumentNullException.ThrowIfNull(option);
+
+            services.Configure(option);
+            services.AddSingleton<ISilmoonConfigureService, SilmoonConfigureService>();
+        }
+    }
+}
