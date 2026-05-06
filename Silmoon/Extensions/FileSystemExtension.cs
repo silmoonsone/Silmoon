1a9023581400ada28d95c3a0ab07d6e1d92bba3a
WEIBIN SONG <sone@silmoon.com>
2026-05-07 02:38:17 +0800

    add FileSystemExtension, release nuget packages [Silmoon 3.0.3], [Silmoon.Extensions 3.0.1].

---
+++ Silmoon/Extensions/FileSystemExtension.cs
@@ -0,0 +1,24 @@
+﻿using Silmoon.IO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Silmoon.Extensions
+{
+    public static class FileSystemExtension
+    {
+#if NET10_0_OR_GREATER
+
+        extension(File)
+        {
+
+        }
+        extension(Directory)
+        {
+            public static void CreateDirectory(string directoryPath) => FileHelper.CreateDirectory(directoryPath);
+            public static void ClearEmptyDirectory(string targetDirectoryPath, string reserveDirectoryPath) => FileHelper.ClearEmptyDirectory(targetDirectoryPath, reserveDirectoryPath);
+        }
+#endif
+    }
+}
